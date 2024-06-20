1f1d360a532c38e2a27904349bc74290868279e4
pkbceira03 <cabeceira2003@gmail.com>
2024-06-20 13:38:37 -0300

    colocando a tipo dos pokemons

--- pokedex2.0/app/components/cartPokemon/cartPokemon.tsx
+++ pokedex2.0/app/components/cartPokemon/cartPokemon.tsx
@@ -5,19 +5,65 @@ import axios from "axios";
 import styles from "./styles.module.css"
 
 export default function CartPokemon(props:any){
-    const [imagem, setImagem] = useState<any>();
+    const [imagem, setImagem] = useState<string>();
+    const [typePokemon, setTypePokemon] = useState<any[]>([]);
+    const [cardColor, setCardColor] = useState<string>("#fff");
+    const [secondyColor, setSecondyColor] = useState<string>("#fff");
+
+    const typeColors: { [key: string]: string } = {
+        normal: "#A8A878",
+        fire: "#F08030",
+        water: "#6890F0",
+        grass: "#78C850",
+        electric: "#F8D030",
+        ice: "#98D8D8",
+        fighting: "#C03028",
+        poison: "#A040A0",
+        ground: "#E0C068",
+        flying: "#A890F0",
+        psychic: "#F85888",
+        bug: "#A8B820",
+        rock: "#B8A038",
+        ghost: "#705898",
+        dragon: "#7038F8",
+        dark: "#705848",
+        steel: "#B8B8D0",
+        fairy: "#EE99AC",
+    };
+    
+
     async function getPokemon(){
         const response = await axios.get(props.urlPokemon);
         setImagem(response.data.sprites.front_default);
+        setTypePokemon(response.data.types);
+
+        if (response.data.types.length > 0) {
+            const primaryType = response.data.types[0].type.name;
+            setCardColor(typeColors[primaryType] || "#fff");
+            const secondyType = response.data.types[1].type.name;
+            setSecondyColor(typeColors[secondyType] || "#fff");
+        }
+
     }
+
     useEffect(()=>{
         getPokemon();
+        console.log(typePokemon)
     }, [])
 
     return(
-        <div className={styles.card}>
+        <div className={styles.card} style={{ backgroundColor: cardColor }}>
             <img src={imagem}></img>
             <p>{props.name}</p>
+            <div className={styles.typePokemon}>
+            {typePokemon.map((val:any, index: number)=>{
+                return <p 
+                            style={{ backgroundColor: index === 0 ? secondyColor : cardColor }}
+                        >
+                            {val.type.name}
+                        </p>
+            })}
+            </div>
         </div>
     )
 }
--- pokedex2.0/app/pages/pokedex/page.tsx
+++ pokedex2.0/app/pages/pokedex/page.tsx
@@ -3,51 +3,54 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import CartPokemon from "@/app/components/cartPokemon/cartPokemon";
-
 import styles from "./styles.module.css"
 
-
-const NEXT_PUBLIC_API_URL= 'https://pokeapi.co/api/v2/pokemon?limit=50'
-
-// const getPokemon = async () =>{
-//     const response = await axios.get(NEXT_PUBLIC_API_URL);
-//     //setPokemon = response.data;
-//     return response.data;
-// }
+const LIMIT = 20; // Definindo quantos Pokémon serão exibidos por página
 
 export default function Pokedex(){
-    const [pokemon, setPokemon] = useState<any>();
-    async function getPokemon(){
-        const reponse = await axios.get(NEXT_PUBLIC_API_URL);
-        const data = await reponse.data;
-        setPokemon(data);
+    const [pokemon, setPokemon] = useState<any>([]);
+    const [offset, setOffset] = useState(0);
+    const [total, setTotal] = useState(0);
+
+    async function getPokemon(offset: number){
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}&offset=${offset}`);
+        const data = response.data;
+        setPokemon(data.results);
+        setTotal(data.count);
     }
-    useEffect(()=>{
-        getPokemon();
-    }, [])
-    
-    //sprities.frontdefault
-    
+
+    useEffect(() => {
+        getPokemon(offset);
+    }, [offset]);
+
+    const handleNextPage = () => {
+        setOffset(prevOffset => Math.min(prevOffset + LIMIT, total - LIMIT));
+    };
+
+    const handlePrevPage = () => {
+        setOffset(prevOffset => Math.max(prevOffset - LIMIT, 0));
+    };
+
     return(
         <div>
-           <div className={styles.center}>
-                {pokemon ?
-                    pokemon.results.map((val:any) => {
-                        return(
-                            <CartPokemon name={val.name} urlPokemon={val.url}/>
-                        )
-                    })
+            <div className={styles.center}>
+                {pokemon.length > 0 ? 
+                    pokemon.map((val:any) => (
+                        <CartPokemon key={val.name} name={val.name} urlPokemon={val.url}/>
+                    ))
                     : (
                         <p>Loading...</p>
                     )
-                
                 }
-           </div>
-            {/* {pokemon ? 
-                pokemon.results.map((val:any) => { return <p>{val.name}</p> })
-             : (
-                <p>Loading...</p>
-            )} */}
+            </div>
+            <div className={styles.pagination}>
+                <button className={styles.btn} onClick={handlePrevPage} disabled={offset === 0}>
+                    Previous
+                </button>
+                <button className={styles.btn} onClick={handleNextPage} disabled={offset + LIMIT >= total}>
+                    Next
+                </button>
+            </div>
         </div>
     );
 }
